b16d62ef409395089140ef39b70a95e8f0b294fd
Aian Lucas <lucas2.aian7@gmail.com>
2025-09-25 16:25:21 -0300

    Upload dos Documentos

---
+++ src/app/componentes/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
+
+@Component({
+  selector: 'app-footer',
+  imports: [RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
---
+++ src/app/componentes/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  onSubmit() {
+    
+  }
+
+}
